d4d0dce42f8e45d153f44aa9e4b07867876ac5f2
John Beynon <john@beynon.org.uk>
2013-07-31 15:02:56 +0100

    Now working as it should

--- workflow/main.rb
+++ workflow/main.rb
@@ -1,45 +1,31 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+def console_log(msg)
+  escape = proc{ |m| m.gsub("'", "'\\\\''") }
+  `logger -t 'Alfred Workflow' '#{escape[msg]}'`
+end
+
+console_log('Executing main.rb!')
+
 require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
+console_log('ruby gems required')
 require "bundle/bundler/setup"
+console_log('bundler required')
 require "alfred"
-
-
-
+console_log('alfred required')
 
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
-
-  # add a file feedback
-  fb.add_file_item(File.expand_path "~/Applications/")
 
   # add an arbitrary feedback
   fb.add_item({
     :uid      => ""                     ,
     :title    => "Just a Test"          ,
-    :subtitle => "feedback item"        ,
+    :subtitle => "#{Time.now}"        ,
     :arg      => "A test feedback Item" ,
     :valid    => "yes"                  ,
   })
   
-  # add an feedback to test rescue feedback
-  fb.add_item({
-    :uid          => ""                     ,
-    :title        => "Rescue Feedback Test" ,
-    :subtitle     => "rescue feedback item" ,
-    :arg          => ""                     ,
-    :autocomplete => "failed"               ,
-    :valid        => "no"                   ,
-  })
-
-  if ARGV[0].eql? "failed"
-    alfred.with_rescue_feedback = true
-    raise Alfred::NoBundleIDError, "Wrong Bundle ID Test!"
-  end
-
   puts fb.to_xml(ARGV)
 end
-
-
-
